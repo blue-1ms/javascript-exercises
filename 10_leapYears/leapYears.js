fd9d403dba1378ca483f9116f098504059760bde
blue-1ms <19849833+blue-1ms@users.noreply.github.com>
2025-07-23 17:10:31 +1000

    complete 06 - 11

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (typeof year !== 'number' || year < 0) {
+        return false; // Invalid input
+    }
+    if (year % 4 === 0) {       
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true; // Divisible by 400
+            }
+            return false; // Divisible by 100 but not by 400
+        }
+        return true; // Divisible by 4 but not by 100
+    }
+    return false; // Not divisible by 4
 };
 
 // Do not edit below this line
